730a65d9b288809e6ba3b762a862aa7e6471a825
PEMaldonado <pablomaldonado2086@gmail.com>
2023-08-06 11:19:57 -0300

    feature-activities finished

--- api/src/controllers/activitiesController.js
+++ api/src/controllers/activitiesController.js
@@ -3,18 +3,18 @@ import {
   createActivity,
 } from "../services/activitiesService.js";
 
-const getActivities = (req, res) => {
+const getActivities = async (req, res) => {
   try {
-    const activities = getAllActivities();
+    const activities = await getAllActivities();
     res.status(200).json(activities);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
-const postActivity = (req, res) => {
+const postActivity = async (req, res) => {
   try {
-    createActivity(req.body);
+    await createActivity(req.body);
     res.status(200).json({ message: "Activity created succesfully " });
   } catch (error) {
     res.status(400).json({ error: error.message });
--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,18 +1,22 @@
 import { DataTypes } from "sequelize";
 
 export default (sequelize) =>
-  sequelize.define("Activity", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+  sequelize.define(
+    "Activity",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: { type: DataTypes.STRING, allowNull: false },
+      difficulty: { type: DataTypes.INTEGER, allowNull: false },
+      duration: { type: DataTypes.INTEGER },
+      season: {
+        type: DataTypes.ENUM("Summer", "Autumn", "Winter", "Spring"),
+        allowNull: false,
+      },
     },
-    name: { type: DataTypes.STRING, allowNull: false },
-    difficulty: { type: DataTypes.INTEGER, allowNull: false },
-    duration: { type: DataTypes.INTEGER },
-    season: {
-      type: DataTypes.ENUM("Summer,Autumn,Winter,Spring"),
-      allowNull: false,
-    },
-  });
+    { timestamps: false }
+  );
